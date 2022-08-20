4a69db9862ff733448e2f3f540c50a4e2688bbd5
jabu <jabu@gx.ag>
2022-08-20 16:43:50 -0700

    better maths

--- src/common/math.rs
+++ src/common/math.rs
@@ -1,9 +1,16 @@
-pub trait GameMath {
+pub trait Lerp {
     fn lerp(&self, start: Self, end: Self) -> Self;
 }
 
-impl GameMath for f32 {
+impl Lerp for f32 {
     fn lerp(&self, start: f32, end: f32) -> f32 {
         start + (end - start) * *self
     }
+}
+
+pub struct Rect<T> {
+    pub left: T,
+    pub right: T,
+    pub bottom: T,
+    pub top: T,
 }
